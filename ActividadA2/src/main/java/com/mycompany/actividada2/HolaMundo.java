68613270ef4a7d4e3df3921f703c4dd13e880103
dam <dam@203P01>
2023-02-09 09:52:25 +0100

    v 2.1

---
+++ ActividadA2/src/main/java/com/mycompany/actividada2/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividada2;
+
+/**
+ *
+ * @author dam
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Prueba Git");
+        System.out.println("edit 1"); // edit 1º usuario
+        System.out.println("edit 2.1"); //edit simultaneo 2º usuario
+    }
+}
